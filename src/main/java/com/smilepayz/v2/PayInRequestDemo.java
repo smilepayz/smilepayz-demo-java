ef41534441bfca0d2fa3fabb1eb98cd8607df54e
moore <moore@tradev-group.com>
2024-07-03 17:57:56 +0800

    comit

--- src/main/java/com/smilepayz/v2/PayInRequestDemo.java
+++ src/main/java/com/smilepayz/v2/PayInRequestDemo.java
@@ -5,6 +5,7 @@ import com.smilepayz.v2.bean.MerchantReq;
 import com.smilepayz.v2.bean.MoneyReq;
 import com.smilepayz.v2.bean.TradePayinReq;
 import com.smilepayz.v2.data.AreaEnum;
+import com.smilepayz.v2.data.Constant;
 import lombok.SneakyThrows;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -20,7 +21,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.UUID;
 
 /**
  * @Author Moore
@@ -28,21 +28,15 @@ import java.util.UUID;
  **/
 public class PayInRequestDemo {
 
-    private static final String privateKeyStr = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3LbmfPhjGXJ+a6NVKyRWEfCpsKfl9UFnMRltNosJv+7qd6OUK3t7Q8fKX8rxuJBeXLLWrOZvjoGlP3ybvvhUSGKH+BLcN1k2eJbcKuThvXvKQvd7pXolY91gkF8V78FX+TKegZupbeji0XUGXCBNyShocYM6Cvailf0Iyv49dcktJmi4drBKfgj5l4HUY9TaDuCZOrhvFZfZZxBK1zkm72k0ZLfqTGmG8O2tsByndCTH2aPLT9odR8/O4qfoTG+vV9HivIKKOI2h0kZfQBOqWD4/ofwu9PNoRRzGgzfRpF3GhnKa3bRVSpCUuBtHfotQfFxbjaDk0s3K5BhQE7HxzAgMBAAECggEARHhaBxUeC595pVzcxVyOp3wGG3JBKL9NIZc277kj9tngcsAoRTzziqS1qmh4WK8zBjYXHg6ln5tJYiqmkjy6AY6llp7KkeiGENRGLEL5vl9+Se4/EXpd2pxyHOOp1N8MNccPbVyqw1DXO0wUhVDme/UI94yUBLjB/kKoSvHhs+qwJ8cz9C8sF3Zs/zF6Te/f+Z+HrIbVj4vlx6DYBs3pWe7J+XYg3XXpbvIBJVTN0lreQAjtopic0F7o1EULnllJmOqy+kiRMuSi5ESrWFOnuCrY0iz/C8LJKlqWa3d+1jVLPvKYucXvddYrwjyu8kYHZAenKWKkLPlQkQxGUFcX8QKBgQDYSCsgg7tDlXTw1P9Z2BhkVo1ugYWA1FcxqTY2sTOMOwCTt3QZAFEqofbNvdjZbk7vu8gh5LyxbWAr/sNqOXH5915a6DZKBGl+qBTH2TZZKmhCUWlmn8T7ySYclfsgIZsUxHaDfY+otiXGtNegSX9US9/AFVSMdiQBCmr5/i48mQKBgQDY0U5+MDl6GnbDH/hCT+YMq/m5W58m7ehJfG6jPtLQkPPvMq2Oj5HUeXx4vDdUxzaRlC9fICwe2KgKiGYpJsRPte6JfO6te7wsO2lDk0oBiw+jewm4CwZ5KeDYyReha8Hc2H1j5hllVx5DIZiage2ZswS5+kCNgzf4QbdAOd886wKBgAo5TyCYWY/WTtLbnr6GgpCrrr/ci40NfJmyYAex1Lf6Sgqxj2FnLG8RfPM42DlfB4g5njpL78eLXhJ2VpJ86LBiSymM9JQHJV2BYIoZ8IHCiW8pHgxl3Q/x8EVFqbtZG1Wd++Q3WUUmZx6/ibnf/47ij08rMvX417bc4TW0GEdxAoGAPX3nUBynQH0e76oyg8QbT766nZphoe3ZcnYK/tuDeMmTlWR/Gq6XQnaOGcPvwWiajmFDqiv6t2jlB8+1gbhP9vd3RqEbJDKypKzY5uRwGc3xyoDLudnOpTB+Z51oyUxBeDwiG+IXk8lIeOufVzrAQ1YlYgWap0fu6MbijSGcsa8CgYEA0OolBxRKdBSxmwMbXDocg5HtHd15UgJdlgL9GFjDZPisUvM7LWK97E5QPhY2IEIcv0EW9jM62bc1uPMhwDuN1dzmLafQIivGsIj5qUy18CoE4KJHsTeKLeHkrd6/nBd4da4A37+ksJ1t/sgZpjl40ShrxTV+OtzzY/kuQ4Uqc9g=";
-    private static final String publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAty25nz4YxlyfmujVSskVhHwqbCn5fVBZzEZbTaLCb/u6nejlCt7e0PHyl/K8biQXlyy1qzmb46BpT98m774VEhih/gS3DdZNniW3Crk4b17ykL3e6V6JWPdYJBfFe/BV/kynoGbqW3o4tF1BlwgTckoaHGDOgr2opX9CMr+PXXJLSZouHawSn4I+ZeB1GPU2g7gmTq4bxWX2WcQStc5Ju9pNGS36kxphvDtrbAcp3Qkx9mjy0/aHUfPzuKn6Exvr1fR4ryCijiNodJGX0ATqlg+P6H8LvTzaEUcxoM30aRdxoZymt20VUqQlLgbR36LUHxcW42g5NLNyuQYUBOx8cwIDAQAB";
-
     @SneakyThrows
     public static void main(String[] args) {
         System.out.println("=====>Payin transaction");
 
         //url
         String endPointUlr = "/v2.0/transaction/pay-in";
-        String sandboxPath = "https://sandbox-gateway-test.smilepayz.com" + endPointUlr;
-        String prodPath = "https://gateway-test.smilepayz.com" + endPointUlr;
-        String sandboxMerchantId = "sandbox-20019";
-        String prodMerchantId = "20019";
-        String merchantSecretSandbox = "6a58a603e5043290f4097ee4a7745661b3656932d4eebc3106b5dddc3af6e053";
-        String merchantSecret = "95b57c46b8c2e068982be23fb669a80612cad68e6ce6ba4f5af9ec20d23bb274";
+        String sandboxPath = Constant.baseUrlSanbox + endPointUlr;
+        String prodPath = Constant.baseUrl + endPointUlr;
+
 
         System.out.println("pay in request url = " + sandboxPath);
 
@@ -55,7 +49,7 @@ public class PayInRequestDemo {
         AreaEnum areaEnum = AreaEnum.INDONESIA;
 
         //generate parameter
-        String merchantOrderNo = "T_" + System.currentTimeMillis();
+        String merchantOrderNo = Constant.merchantId + System.currentTimeMillis();
         String purpose = "Purpose For Transaction from Java SDK";
         String paymentMethod = "W_DANA";
 
@@ -65,7 +59,7 @@ public class PayInRequestDemo {
         moneyReq.setAmount(amount);
         //merchantReq
         MerchantReq merchantReq = new MerchantReq();
-        merchantReq.setMerchantId(sandboxMerchantId);
+        merchantReq.setMerchantId(Constant.merchantIdSandBox);
 
         TradePayinReq payinReq = new TradePayinReq();
         payinReq.setOrderNo(merchantOrderNo);
@@ -87,8 +81,8 @@ public class PayInRequestDemo {
         System.out.println("minify = " + minify);
 
         //signature
-        String content = String.join("|",  timestamp, merchantSecret,minify);
-        String signature = SignatureUtils.sha256RsaSignature(content, privateKeyStr);
+        String content = String.join("|", timestamp, Constant.merchantSecretSandBox, minify);
+        String signature = SignatureUtils.sha256RsaSignature(content, Constant.privateKeyStr);
 
 
         // create httpClient
@@ -97,7 +91,7 @@ public class PayInRequestDemo {
         httpPost.addHeader("Content-Type", "application/json");
         httpPost.addHeader("X-TIMESTAMP", timestamp);
         httpPost.addHeader("X-SIGNATURE", signature);
-        httpPost.addHeader("X-PARTNER-ID", sandboxMerchantId);
+        httpPost.addHeader("X-PARTNER-ID", Constant.merchantIdSandBox);
 
         // set entity
         httpPost.setEntity(new StringEntity(jsonStr, StandardCharsets.UTF_8));
@@ -114,7 +108,6 @@ public class PayInRequestDemo {
         EntityUtils.consume(httpEntity);
 
         System.out.println("The pay interface is completed, and you can get your payment link");
-
 
 
     }
